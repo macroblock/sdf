797f328150d341749920521044b4f2b397c7190d
macroblock <undefined.subject@gmail.com>
2019-04-22 21:08:38 +1000

    sync

--- pkg/ui/theme.go
+++ pkg/ui/theme.go
@@ -2,29 +2,51 @@ package ui
 
 import (
 	"image/color"
-
-	"github.com/macroblock/sdf/pkg/resources"
 )
 
-// ITheme -
-type ITheme interface {
-	BackgroundColor() color.Color
-	ButtonColor() color.Color
-	HyperlinkColor() color.Color
-	TextColor() color.Color
-	PlaceHolderColor() color.Color
-	PrimaryColor() color.Color
-	FocusColor() color.Color
-	ScrollBarColor() color.Color
+type (
+	// ITheme -
+	ITheme interface {
+		Color(ColorIndex) color.Color
 
-	TextSize() int
-	TextFont() resources.IResource
-	TextBoldFont() resources.IResource
-	TextItalicFont() resources.IResource
-	TextBoldItalicFont() resources.IResource
-	TextMonospaceFont() resources.IResource
+		// TextSize() int
+		// TextFont() resources.IResource
+		// TextBoldFont() resources.IResource
+		// TextItalicFont() resources.IResource
+		// TextBoldItalicFont() resources.IResource
+		// TextMonospaceFont() resources.IResource
 
-	Padding() int
-	IconInlineSize() int
-	ScrollBarSize() int
+		// Padding() int
+		// IconInlineSize() int
+		// ScrollBarSize() int
+	}
+
+	// ColorIndex -
+	ColorIndex int
+
+	builtinTheme [maxColors]color.Color
+)
+
+// -
+const (
+	BackgroundColor ColorIndex = iota
+	ButtonColor
+	HyperlinkColor
+	TextColor
+	PlaceHolderColor
+	PrimaryColor
+	FocusColor
+	ScrollBarColor
+	maxColors
+)
+
+var builtinDarkTheme = [maxColors]color.Color{
+	// Dark theme -
+	color.RGBA{0x42, 0x42, 0x42, 0xff},
+	color.RGBA{0x21, 0x21, 0x21, 0xff},
+	color.RGBA{0xff, 0xff, 0xff, 0xff},
+	color.RGBA{0x99, 0x99, 0xff, 0xff},
+	color.RGBA{0xb2, 0xb2, 0xb2, 0xff},
+	color.RGBA{0x1a, 0x23, 0x7e, 0xff},
+	color.RGBA{0x0, 0x0, 0x0, 0x99},
 }
